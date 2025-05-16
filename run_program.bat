3b1dbacfc9b2540524babec4458f1a042d0f5f3a
Khairul <khairilaiman1@gmail.com>
2025-05-16 15:42:12 +0800

    Added .bat file for Windows execution

---
+++ run_program.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+@REM start cmd /k "echo Opening new Command Prompt terminal"
+
+set VENV_DIR=myenv
+
+echo Checking if virtual environment exists...
+if exist "%VENV_DIR%" (
+    echo Yes, Virtual environment already exists: %VENV_DIR%
+) else (
+    echo No, Virtual environment does not exist. Creating virtual environment...
+    python -m venv %VENV_DIR%
+)
+
+echo Activating virtual environment...
+call .\myenv\Scripts\activate.bat
+
+echo Checking Python executable...
+where python
+
+call pip install opencv-python
+python cam_capture.py
+
+del "%VENV_DIR%"
+
+pause
